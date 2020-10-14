91ae2423cae877a7a26519623a275f2125dc3034
ChwastKakol <piotrek.krzysiek.kakol@gmail.com>
2020-10-14 13:42:10 +0200

    goo edit 2

--- src/Foo.java
+++ src/Foo.java
@@ -6,7 +6,7 @@ public class Foo {
         System.out.println("Foooooo");
     }
 
-    public void goo() {System.out.println("Goo is no more");}
+    public void goo() {System.out.println("Goo is again");}
 
     public void loo() {System.out.println("Loo");}
 
